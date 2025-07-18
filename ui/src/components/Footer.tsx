2a3a7b8479db1f03bdb76105d598b00e0ddce69c
Sergey A Oskorbin <soskorbin@alfabank.ru>
2025-07-18 12:52:29 +0300

    Fix footer (project name) and link to GitHub

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -5,9 +5,9 @@ const version = packageJson.version;
 
 export const Footer: React.FC = () => (
     <footer className="w-full py-4 text-center text-sm text-neutral-500 border-t border-neutral-800 bg-neutral-950">
-        © 2025 Simple Log Viewer &nbsp;|&nbsp; v{version} | &nbsp;
+        © 2025 Simple Syslog Viewer &nbsp;|&nbsp; v{version} | &nbsp;
         <a
-            href="https://github.com/sergo44/simple-sysloo-viewer"
+            href="https://github.com/sergo44/simple-syslog-viewer"
             className="text-violet-400 hover:underline"
             target="_blank"
             rel="noopener noreferrer"
